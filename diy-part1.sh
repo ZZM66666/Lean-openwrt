6cd56884dbce5cb6cb77aa33f624ab5d6f7d24a1
ZZMHA227 <57690855+ZZMHA227@users.noreply.github.com>
2021-04-17 11:11:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+
